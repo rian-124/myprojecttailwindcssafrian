f477fc11ab2e86b2f7f6cca9bca306d5cd048224
rian-124 <afriyanlionel0@gmail.com>
2025-01-24 16:50:50 +0700

    update-ui-about-me

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,23 +1,75 @@
 // navbar-fixed
 
-window.onscroll = function ( ) {
-    const header = document.querySelector('header');
-    const fixedNav = header.offsetTop;
+// window.onscroll = function ( ) {
+//     const header = document.querySelector('header');
+//     const fixedNav = header.offsetTop;
 
-    if (window.scrollY > fixedNav) {
-        header.classList.add('navbar-fixed');
-    } else {
-        header.classList.remove('navbar-fixed');
+//     if (window.scrollY > fixedNav) {
+//         header.classList.add('navbar-fixed');
+//     } else {
+//         header.classList.remove('navbar-fixed');
+//     }
+// };
+
+// // hamburger
+
+
+// const humberger = document.getElementById('humberger');
+// const navMenu = document.getElementById('nav-menu');
+
+// humberger.addEventListener('click', function() {
+//     humberger.classList.toggle('humberger-active');
+//     navMenu.classList.toggle('hidden');
+// });
+
+//animation
+
+function delay (ms) {
+    return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+function loadUI () {
+    window.onload = async  () => {
+        const title = document.querySelector('.title');
+        const statement = document.querySelector('.statement');
+        const profil_image = document.querySelector('.profil-image');
+        const profil_component = document.querySelector('.profil-component');
+        const button_contact = document.querySelector('.contact-button');
+
+        const transfomTitle = !title.classList.contains('transform-right-title') ? (title.classList.add('transform-right-title')) : title.classList.remove('transform-right-title');
+        await delay(1000);
+        const transformStatement = !statement.classList.contains('transform-right-title') ? (statement.classList.add('transform-right-title')) : statement.classList.remove('transform-right-title');
+        await delay(500);
+        const transformProfil_component = !profil_component.classList.contains('transform-left') ? (profil_component.classList.add('transform-left')) : profil_component.classList.remove('transform-left');
+        await delay(500);
+        const transformProfil_image = !profil_image.classList.contains('tranform-profil') ? (profil_image.classList.add('tranform-profil')) : profil_image.classList.remove('tranform-profil');
+        await delay(500);
+        const tranformButton_contact = !button_contact.classList.contains('contact-button-transform') ? (button_contact.classList.add('contact-button-transform')) : button_contact.classList.remove('contact-button-transform');
+
     }
-};
+}
 
-// hamburger
+function contactButtonOnClick() {
+    const button_contact = document.querySelector('.contact-button');
+    const linkA = document.getElementById('contact-link');
+    const link_Social_Media = document.getElementById('social-media-wa');
+    const link_Social_Media2 = document.getElementById('social-media-instagram');
+
+    
+        button_contact.addEventListener('click', async (e) => {
+        
+            button_contact.classList.toggle('active');
+            linkA.classList.toggle('anu');
+            link_Social_Media.classList.toggle('active');
+            link_Social_Media2.classList.toggle('active');
+           
+        });
+   
+}
 
 
-const humberger = document.getElementById('humberger');
-const navMenu = document.getElementById('nav-menu');
 
-humberger.addEventListener('click', function() {
-    humberger.classList.toggle('humberger-active');
-    navMenu.classList.toggle('hidden');
-});
+
+
+contactButtonOnClick();
+loadUI();
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,9 +7,14 @@ module.exports = {
       padding: '16px', // Padding default
     },
     extend: {
+      fontFamily : {
+        poppins : ['poppins', 'sans-serif'],
+      },
       colors: {
         primary: '#ef4444',
         dark: '#020617',
+        bgGradientpurple: '#755bc9',
+        bgGradientDark: '#1b1b33',
       },
       screens: {
         '2xl': '1320px', // Ukuran untuk breakpoint 2xl (atau sesuai preferensi)
